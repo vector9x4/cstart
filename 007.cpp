8dc093bb5042d42dde8cb93742ea8927004d8a3d
vector9x4 <do012306@gmail.com>
2021-01-11 15:58:33 +0900

    Update 007.cpp

--- 007.cpp
+++ 007.cpp
@@ -2,5 +2,9 @@
 
 int main(void)
 {
-	printf("Loves the Jobs You Hate");
+	printf("Loves the Jobs You Hate"); // C 언어에서 더블 쿼테이션으로 감싼 것을 모두 문자열이라고 한다.
 }
+
+/*
+문자열은 문자들의 모임이며, 더블 쿼테이션으로 감싼다. 후에 배울 포인터와 밀접한 관련이 있다.
+*/
