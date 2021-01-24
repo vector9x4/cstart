b49ef272505f5ce77fd7dd4370a56af6064b15b4
vector9x4 <do012306@gmail.com>
2021-01-24 20:37:17 +0900

    Add files via upload

---
+++ 028_쉼표_연산자_이해하기.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 1, y = 2, max;
+
+	max = x > y ? x : y;
+
+	printf("max = %d, x = %d, y = %d", max, x, y);
+}
+/*
+- 쉼표 연산자는 변수의 정의 시 또는 함수 등에서 사용한다.
+*/
